4d0a63f3e7e13a1bf790bbb03f8aac77d3d4c2cc
chowchiling@gmail.com <chowchiling@gmail.com@3743440c-1c92-ba80-8f5a-e69350aafc75>
2014-11-13 09:49:08 +0000

    ReportGeneration MonitorPanel

--- trunk/scts/src/scts/ui/MonitorPanel.java
+++ trunk/scts/src/scts/ui/MonitorPanel.java
@@ -1,10 +1,13 @@
 package scts.ui;
 
 import java.awt.Color;
+import java.awt.Container;
 import java.awt.FlowLayout;
+import java.awt.LayoutManager;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
@@ -21,21 +24,28 @@ public class MonitorPanel extends JPanel{
 	
 	public MonitorPanel() {
 		
-		super(new FlowLayout());
+		//super(new FlowLayout());
+		
+		JPanel panel = new JPanel();
+		LayoutManager layout = new BoxLayout(panel, BoxLayout.Y_AXIS);
+		panel.setLayout(layout);
+		panel.setBackground(Color.BLACK);
 		
 		configBtn = new JButton("Configuration");
 		reportBtn = new JButton("Operation Report");
 		quitBtn = new JButton("Quit");
 		
-		this.add(configBtn);
-		this.add(reportBtn);
-		this.add(quitBtn);
+		panel.add(configBtn);
+		panel.add(reportBtn);
+		panel.add(quitBtn);
 		
+		this.add(panel);
 		this.setBackground(Color.BLACK);
 		
 		reportBtn.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
+				stats = new Stats();
 				new ReportPanel(stats);
 			}
 		});
